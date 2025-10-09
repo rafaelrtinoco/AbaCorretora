6dde9f2ad8a6200e988602356b21407a26123c12
Rafael Ribeiro Tinoco <rafaelribeiro-sao@braspress.com.br>
2025-10-09 13:44:35 -0300

    configurações de deploy

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:"/AbaCorretora"
+  base:"/AbaCorretora",
    resolve: {
     alias: {
       '@': '/src',
